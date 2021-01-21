16a5d6be7f87abaa822640814c502ab2d53dc599
moriss <marrocco001@gmail.com>
2021-01-21 20:52:34 +0000

    hello world

---
+++ navigate.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <unistd.h>
+//Print 'hello world' to the screen
+
+int main (void)
+{
+   printf("hello world\n");//Check notes for void_input.c file, after each line of code.
+}
+
+
+
+
+

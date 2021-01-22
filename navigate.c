57207a23b6aee439dcc4e154f6e7d863466cf233
moriss <marrocco001@gmail.com>
2021-01-22 09:47:59 +0000

    without letter count

--- navigate.c
+++ navigate.c
@@ -6,22 +6,12 @@
 #include <ctype.h>
 #include <unistd.h>
 //Print 'hello [user input]' to the screen
-
+//without word count
 int main (void)
 {
     char * name = get_string("What is your name?: " );
 
    printf("hello %s\n", name);//Check notes for void_input.c file, after each line of code.
-   int count = 0;
-   for (int i = 0; i < strlen(name); i ++ )
-   {
-       if(name[i] != 32 )
-       {
-           count = count + 1;
-       }
-   }
-   printf("There are %i letters in your name.", count);
-
 }
 
 

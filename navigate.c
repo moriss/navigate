28c4710426346d3962ae191cb21ef2dddc169961
moriss <marrocco001@gmail.com>
2021-01-22 09:44:34 +0000

    user input

--- navigate.c
+++ navigate.c
@@ -5,11 +5,24 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
-//Print 'hello world' to the screen
+//Print 'hello [user input]' to the screen
 
 int main (void)
 {
     char * name = get_string("What is your name?: " );
-    
+
    printf("hello %s\n", name);//Check notes for void_input.c file, after each line of code.
+   int count = 0;
+   for (int i = 0; i < strlen(name); i ++ )
+   {
+       if(name[i] != 32 )
+       {
+           count = count + 1;
+       }
+   }
+   printf("There are %i letters in your name.", count);
+
 }
+
+
+
